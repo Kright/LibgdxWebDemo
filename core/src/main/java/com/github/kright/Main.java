d9f619028a8b9c7b51935b313aecd79528fabddc
Igor Slobodskov <igor.slobodskov@phystech.edu>
2025-07-08 21:22:30 +0200

    add rendering of rotating sphere with 16k triangles

--- core/src/main/java/com/github/kright/Main.java
+++ core/src/main/java/com/github/kright/Main.java
@@ -3,24 +3,92 @@ package com.github.kright;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.VertexAttributes.Usage;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.loader.ObjLoader;
 import com.badlogic.gdx.utils.ScreenUtils;
+
+import java.util.ArrayList;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
+
+    private static final int objectsX = 5;
+    private static final int objectsZ = 5;
+
     private SpriteBatch batch;
     private Texture image;
+
+    // 3D rendering components
+    private PerspectiveCamera camera;
+    private ModelBatch modelBatch;
+    private Model model;
+    private Environment environment;
+    private final ArrayList<ModelInstance> instances = new ArrayList<ModelInstance>();
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         image = new Texture("libgdx.png");
+
+        // Initialize 3D components
+        modelBatch = new ModelBatch();
+
+        // Set up camera
+        camera = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.position.set(5f, 5f, 2f);
+        camera.lookAt(0, 0, 0);
+        camera.near = 0.1f;
+        camera.far = 100f;
+        camera.update();
+
+        // Set up lighting environment
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+
+        // Load the sphere model
+        ObjLoader loader = new ObjLoader();
+        model = loader.loadModel(Gdx.files.internal("Sphere128x64.obj"));
+
+        for (int z = 0; z < objectsZ; z++) {
+            for (int x = 0; x < objectsX; x++) {
+                ModelInstance instance = new ModelInstance(model);
+                instance.transform.translate((x - objectsX * 0.5f) * 2f, 0, (z - objectsZ * 0.5f) * 2f);
+                instances.add(instance);
+            }
+        }
     }
 
     @Override
     public void render() {
+        // Clear the screen and depth buffer
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+        // Update camera
+        camera.update();
+
+        // Render 3D model
+        modelBatch.begin(camera);
+
+        for (ModelInstance instance : instances) {
+            instance.transform.rotate(0, 1, 0, 0.5f);
+            modelBatch.render(instance, environment);
+        }
+
+        modelBatch.end();
+
+        // Render 2D sprite on top if needed
         batch.begin();
         batch.draw(image, 140, 210);
         batch.end();
@@ -28,7 +96,12 @@ public class Main extends ApplicationAdapter {
 
     @Override
     public void dispose() {
+        // Dispose 2D resources
         batch.dispose();
         image.dispose();
+
+        // Dispose 3D resources
+        modelBatch.dispose();
+        model.dispose();
     }
 }
